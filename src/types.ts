42f932576f3aa66f991441b261889fbc472eec73
chezhe <chezhe@hey.com>
2023-08-21 17:03:47 +0800

    pdf reader

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface Outline {
+  title: string
+  bold: boolean
+  italic: boolean
+  /**
+   * - The color in RGB format to use for
+   * display purposes.
+   */
+  color: Uint8ClampedArray
+  dest: string | Array<any> | null
+  url: string | null
+  unsafeUrl: string | undefined
+  newWindow: boolean | undefined
+  count: number | undefined
+  items: any[]
+}
+
+export interface BookInfo {
+  Title: string
+  Author: string
+}
